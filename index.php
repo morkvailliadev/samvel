31f89478c8ee4ab49a157236abc515ebac6f365e
Illia Morkva <dev.oleksa@gmail.com>
2018-02-27 12:48:43 +0200

    replace

--- index.php
+++ index.php
@@ -14,20 +14,19 @@ $mainResult = $db->query('SELECT id, varh, varf, varl, Dateb, dates, datea, date
 
 // Main loop ordered_act_kpp
 
-while ($rowMain = $mainResult->fetch()) {
+while ($r = $mainResult->fetch()) {
 
-    mainAlgorithmForCurrentRow($rowMain, $db);
+    mainAlgorithmForCurrentRow($r, $db);
 
 }
 
 print_r($db);
 
 
-function mainAlgorithmForCurrentRow($rowMain, $db)
+function mainAlgorithmForCurrentRow($r, $db)
 {
-// First algorithm
 
-    if ($rowMain['int1'] == 2) {
+    if ($r['int1'] == 2) {
 
         $firstSql = 'INSERT INTO date1date2 '
             . '(varh, varf, varl, dateb, int1_in, dates_in, datea_in, datev_in, datek_in, dateg_in, int2_in, int3_in, vard_in, vark_in, int1_out, dates_out, 
@@ -38,18 +37,18 @@ function mainAlgorithmForCurrentRow($rowMain, $db)
 
         $result = $db->prepare($sql);
 
-        $result->execute(array(':varh' => $rowMain['varh'], ':varf' => $rowMain['varf'], ':varl' => $rowMain['varl'], ':dateb' => $rowMain['Dateb'], ':int1_in' => $rowMain['int1'],
-            ':dates_in' => $rowMain['dates'], ':datea_in' => $rowMain['datea'], ':datev_in' => $rowMain['datev'], ':datek_in' => $rowMain['datek'], ':dateg_in' => $rowMain['Dateg'],
-            ':int2_in' => $rowMain['int2'], ':int3_in' => $rowMain['int3'], ':vard_in' => $rowMain['vard'], ':vark_in' => $rowMain['vark'], ':int1_out' => $rowMain['int1'],
-            ':dates_out' => $rowMain['dates'], ':datea_out' => $rowMain['datea'], ':datev_out' => $rowMain['datev'], ':datek_out' => $rowMain['datek'], ':dateg_out' => $rowMain['Dateg'],
-            ':int2_out' => $rowMain['int2'], ':int3_out' => $rowMain['int3'], ':vard_out' => $rowMain['vard'], ':vark_out' => $rowMain['vark'], ':ins_date' => $rowMain['dates']));
-    } elseif ($rowMain['int1'] == 1) {
+        $result->execute(array(':varh' => $r['varh'], ':varf' => $r['varf'], ':varl' => $r['varl'], ':dateb' => $r['Dateb'], ':int1_in' => $r['int1'],
+            ':dates_in' => $r['dates'], ':datea_in' => $r['datea'], ':datev_in' => $r['datev'], ':datek_in' => $r['datek'], ':dateg_in' => $r['Dateg'],
+            ':int2_in' => $r['int2'], ':int3_in' => $r['int3'], ':vard_in' => $r['vard'], ':vark_in' => $r['vark'], ':int1_out' => $r['int1'],
+            ':dates_out' => $r['dates'], ':datea_out' => $r['datea'], ':datev_out' => $r['datev'], ':datek_out' => $r['datek'], ':dateg_out' => $r['Dateg'],
+            ':int2_out' => $r['int2'], ':int3_out' => $r['int3'], ':vard_out' => $r['vard'], ':vark_out' => $r['vark'], ':ins_date' => $r['dates']));
+    } elseif ($r['int1'] == 1) {
 
         $sqlInt1 = 'SELECT * FROM date1date2 WHERE varh = :varh AND int1_out = NULL';
 
         $result = $db->prepare($sqlInt1);
 
-        $result->bindParam(':varh', $rowMain['varh'], PDO::PARAM_STR);
+        $result->bindParam(':varh', $r['varh'], PDO::PARAM_STR);
         $result->setFetchMode(PDO::FETCH_ASSOC);
         $result->execute();
 
@@ -67,11 +66,11 @@ function mainAlgorithmForCurrentRow($rowMain, $db)
 
             $result = $db->prepare($sql);
 
-            $result->execute(array(':varh' => $rowMain['varh'], ':varf' => $rowMain['varf'], ':varl' => $rowMain['varl'], ':dateb' => $rowMain['Dateb'], ':int1_in' => $rowMain['int1'],
-                ':dates_in' => $rowMain['dates'], ':datea_in' => $rowMain['datea'], ':datev_in' => $rowMain['datev'], ':datek_in' => $rowMain['datek'], ':dateg_in' => $rowMain['Dateg'],
-                ':int2_in' => $rowMain['int2'], ':int3_in' => $rowMain['int3'], ':vard_in' => $rowMain['vard'], ':vark_in' => $rowMain['vark'], ':int1_out' => $rowMain['int1'],
-                ':dates_out' => $rowMain['dates'], ':datea_out' => $rowMain['datea'], ':datev_out' => $rowMain['datev'], ':datek_out' => $rowMain['datek'], ':dateg_out' => $rowMain['Dateg'],
-                ':int2_out' => $rowMain['int2'], ':int3_out' => $rowMain['int3'], ':vard_out' => $rowMain['vard'], ':vark_out' => $rowMain['vark'], ':ins_date' => $rowMain['dates']));
+            $result->execute(array(':varh' => $r['varh'], ':varf' => $r['varf'], ':varl' => $r['varl'], ':dateb' => $r['Dateb'], ':int1_in' => $r['int1'],
+                ':dates_in' => $r['dates'], ':datea_in' => $r['datea'], ':datev_in' => $r['datev'], ':datek_in' => $r['datek'], ':dateg_in' => $r['Dateg'],
+                ':int2_in' => $r['int2'], ':int3_in' => $r['int3'], ':vard_in' => $r['vard'], ':vark_in' => $r['vark'], ':int1_out' => $r['int1'],
+                ':dates_out' => $r['dates'], ':datea_out' => $r['datea'], ':datev_out' => $r['datev'], ':datek_out' => $r['datek'], ':dateg_out' => $r['Dateg'],
+                ':int2_out' => $r['int2'], ':int3_out' => $r['int3'], ':vard_out' => $r['vard'], ':vark_out' => $r['vark'], ':ins_date' => $r['dates']));
         } else {
             $secondSql = 'INSERT INTO date1date2 '
                 . '(varh, varf, varl, dateb, int1_in, dates_in, datea_in, datev_in, datek_in, dateg_in, int2_in, int3_in, vard_in, vark_in, int1_out, dates_out, 
@@ -82,11 +81,11 @@ function mainAlgorithmForCurrentRow($rowMain, $db)
 
             $result = $db->prepare($sql);
 
-            $result->execute(array(':varh' => $rowMain['varh'], ':varf' => $rowMain['varf'], ':varl' => $rowMain['varl'], ':dateb' => $rowMain['Dateb'], ':int1_in' => $rowMain['int1'],
-                ':dates_in' => $rowMain['dates'], ':datea_in' => $rowMain['datea'], ':datev_in' => $rowMain['datev'], ':datek_in' => $rowMain['datek'], ':dateg_in' => $rowMain['Dateg'],
-                ':int2_in' => $rowMain['int2'], ':int3_in' => $rowMain['int3'], ':vard_in' => $rowMain['vard'], ':vark_in' => $rowMain['vark'], ':int1_out' => $rowMain['int1'],
-                ':dates_out' => $rowMain['dates'], ':datea_out' => $rowMain['datea'], ':datev_out' => $rowMain['datev'], ':datek_out' => $rowMain['datek'], ':dateg_out' => $rowMain['Dateg'],
-                ':int2_out' => $rowMain['int2'], ':int3_out' => $rowMain['int3'], ':vard_out' => $rowMain['vard'], ':vark_out' => $rowMain['vark'], ':ins_date' => $rowMain['dates']));
+            $result->execute(array(':varh' => $r['varh'], ':varf' => $r['varf'], ':varl' => $r['varl'], ':dateb' => $r['Dateb'], ':int1_in' => $r['int1'],
+                ':dates_in' => $r['dates'], ':datea_in' => $r['datea'], ':datev_in' => $r['datev'], ':datek_in' => $r['datek'], ':dateg_in' => $r['Dateg'],
+                ':int2_in' => $r['int2'], ':int3_in' => $r['int3'], ':vard_in' => $r['vard'], ':vark_in' => $r['vark'], ':int1_out' => $r['int1'],
+                ':dates_out' => $r['dates'], ':datea_out' => $r['datea'], ':datev_out' => $r['datev'], ':datek_out' => $r['datek'], ':dateg_out' => $r['Dateg'],
+                ':int2_out' => $r['int2'], ':int3_out' => $r['int3'], ':vard_out' => $r['vard'], ':vark_out' => $r['vark'], ':ins_date' => $r['dates']));
         }
     }
 }
