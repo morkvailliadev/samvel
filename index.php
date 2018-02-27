04fcf1d3b7ad9ac164e26dbcf4f3c9006222dfa3
Illia Morkva <dev.oleksa@gmail.com>
2018-02-27 12:53:45 +0200

    add new structure method

--- index.php
+++ index.php
@@ -9,18 +9,24 @@ include_once ROOT . '/components/Db.php';
 $db = Db::getConnection();
 
 
+/* Main logic */
+
 $mainResult = $db->query('SELECT id, varh, varf, varl, Dateb, dates, datea, datek, datev, Dateg, int1, int2, vark, vard, int3 
                                       FROM odered_act_kpp ORDER BY id DESC');
 
-// Main loop ordered_act_kpp
 
 while ($r = $mainResult->fetch()) {
 
+    //Call 1 method
+
+    //Call 2 method
     mainAlgorithmForCurrentRow($r, $db);
+
+    //Call 3 method
 
 }
 
-print_r($db);
+/* End logic*/
 
 
 function mainAlgorithmForCurrentRow($r, $db)
@@ -28,7 +34,7 @@ function mainAlgorithmForCurrentRow($r, $db)
 
     if ($r['int1'] == 2) {
 
-        $firstSql = 'INSERT INTO date1date2 '
+        $sql = 'INSERT INTO date1date2 '
             . '(varh, varf, varl, dateb, int1_in, dates_in, datea_in, datev_in, datek_in, dateg_in, int2_in, int3_in, vard_in, vark_in, int1_out, dates_out, 
             datea_out, datev_out, datek_out, dateg_out, int2_out, int3_out, vard_out, vark_out, oshibka, ins_date'
             . 'VALUES '
@@ -44,9 +50,9 @@ function mainAlgorithmForCurrentRow($r, $db)
             ':int2_out' => $r['int2'], ':int3_out' => $r['int3'], ':vard_out' => $r['vard'], ':vark_out' => $r['vark'], ':ins_date' => $r['dates']));
     } elseif ($r['int1'] == 1) {
 
-        $sqlInt1 = 'SELECT * FROM date1date2 WHERE varh = :varh AND int1_out = NULL';
+        $sql = 'SELECT * FROM date1date2 WHERE varh = :varh AND int1_out = NULL';
 
-        $result = $db->prepare($sqlInt1);
+        $result = $db->prepare($sql);
 
         $result->bindParam(':varh', $r['varh'], PDO::PARAM_STR);
         $result->setFetchMode(PDO::FETCH_ASSOC);
@@ -57,7 +63,7 @@ function mainAlgorithmForCurrentRow($r, $db)
         // В таблице date1date2 есть строка в которой varh=p_varh и int1_out is null
         if ($id) {
 
-            $firstSql = 'INSERT INTO date1date2 '
+            $sql = 'INSERT INTO date1date2 '
                 . '(varh, varf, varl, dateb, int1_in, dates_in, datea_in, datev_in, datek_in, dateg_in, int2_in, int3_in, vard_in, vark_in, int1_out, dates_out, 
             datea_out, datev_out, datek_out, dateg_out, int2_out, int3_out, vard_out, vark_out, oshibka, ins_date'
                 . 'VALUES '
@@ -72,7 +78,7 @@ function mainAlgorithmForCurrentRow($r, $db)
                 ':dates_out' => $r['dates'], ':datea_out' => $r['datea'], ':datev_out' => $r['datev'], ':datek_out' => $r['datek'], ':dateg_out' => $r['Dateg'],
                 ':int2_out' => $r['int2'], ':int3_out' => $r['int3'], ':vard_out' => $r['vard'], ':vark_out' => $r['vark'], ':ins_date' => $r['dates']));
         } else {
-            $secondSql = 'INSERT INTO date1date2 '
+            $sql = 'INSERT INTO date1date2 '
                 . '(varh, varf, varl, dateb, int1_in, dates_in, datea_in, datev_in, datek_in, dateg_in, int2_in, int3_in, vard_in, vark_in, int1_out, dates_out, 
             datea_out, datev_out, datek_out, dateg_out, int2_out, int3_out, vard_out, vark_out, oshibka, ins_date'
                 . 'VALUES '
