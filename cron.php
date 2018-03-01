ece15dec56d73896b00c27370ccdeb3d8b6fbbcd
alexey <developer.kulikov@gmail.com>
2018-03-01 13:21:38 +0200

    added comments to ccron.php

--- cron.php
+++ cron.php
@@ -3,5 +3,8 @@
 $output = shell_exec('ps -C php -f');
 
 if (strpos($output, "php index.php") === false) {
+    echo 'запускаем файл';
     shell_exec('php index.php  > /dev/null 2>&1 &');
+} else {
+    echo 'файл все еще запущен ... ';
 }
